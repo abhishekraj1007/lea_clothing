b6e7d50faab8a07c8664b9d658b6fba00e7c27ab
Chandan Roy <chandan.roy@algoscale.com>
2022-05-24 12:56:27 +0530

    Quiz floating button added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import {
 //   Card,
 //   Page,
@@ -15,9 +15,13 @@ import { useEffect } from "react";
 
 // import { ProductsCard } from "./ProductsCard";
 import LeaQuiz from "../pages/LeaQuiz/LeaQuiz";
-import { Box } from "@mui/material";
+import { Box, Button, Fab } from "@mui/material";
 
 export function HomePage() {
+  const [popupBtn, setPopupBtn] = useState(false);
+  const handleModal = () => {
+    setPopupBtn(!popupBtn);
+  };
   // useEffect(() => {
   //   fetch("https://www.google.com", {
   //     method: "GET",
@@ -32,7 +36,25 @@ export function HomePage() {
 
   return (
     <Box>
-      <LeaQuiz />
+      <Box
+        sx={{ position: "fixed", bottom: "24px", right: "24px", zIndex: "10" }}
+      >
+        <Fab
+          sx={{
+            backgroundColor: "#FFE6F6",
+            color: "#000",
+            "&:hover": {
+              backgroundColor: "#6C4A6D",
+              color: "#fff",
+              transition: "all 0.4",
+            },
+          }}
+          onClick={handleModal}
+        >
+          {"Quiz"}
+        </Fab>
+      </Box>
+      {popupBtn && <LeaQuiz />}
     </Box>
   );
 }
